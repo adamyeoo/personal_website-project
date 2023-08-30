d589d6d4d057f084fd6f591f028a15189fd370d7
Adam Yeo <addamyeo@gmail.com>
2023-08-30 16:58:01 -0400

    Implement media files

--- data_proj/models.py
+++ data_proj/models.py
@@ -4,6 +4,8 @@ class DataProj(models.Model):
     proj_title = models.CharField()
     image = models.ImageField(upload_to='images/')
     summary = models.CharField()
+    github = models.CharField()
+    devpost = models.CharField()
 
     # puts name for blogs in admin page (more readable)
     def __str__(self):
